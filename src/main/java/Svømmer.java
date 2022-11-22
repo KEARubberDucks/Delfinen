9aa4fa4c49a942524c43771601569da7249e75db
HostilePlayer <mart5e86@stud.kea.dk>
2022-11-22 10:41:04 +0100

    svømmer contructer

--- src/main/java/Svømmer.java
+++ src/main/java/Svømmer.java
@@ -1,2 +1,18 @@
 public class Svømmer {
+
+    String name;
+    int age;
+    boolean isActive;
+    boolean isjunior;
+    boolean isSeniors;
+    boolean competetiv;
+
+     Svømmer (String name, int age, boolean isActive, boolean isjunior, boolean isSeniors, boolean competetiv){
+         this.name = name;
+         this.age = age;
+         this.isActive = isActive;
+         this.isjunior = isjunior;
+         this.isSeniors = isSeniors;
+         this.competetiv = competetiv;
+     }
 }
