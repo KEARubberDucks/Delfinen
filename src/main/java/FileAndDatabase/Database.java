1b62d125a9de9633ec91cb7d6d5785bb0735a598
Tohasim <toreh.simonsen@live.dk>
2022-11-24 10:22:00 +0100

    Fixed main so it is working

--- src/main/java/FileAndDatabase/Database.java
+++ src/main/java/FileAndDatabase/Database.java
@@ -16,4 +16,8 @@ public class Database {
     public void initSwimmers(ArrayList<Swimmer> swimmers) {
         this.swimmers = swimmers;
     }
+
+    public void createSvømmer(String name, int age, boolean isActive, boolean competetiv) {
+        swimmers.add(new Swimmer(name, age, isActive, competetiv));
+    }
 }
--- src/main/java/MainClasses/Controller.java
+++ src/main/java/MainClasses/Controller.java
@@ -104,7 +104,7 @@ public class Controller {
 
             }
         }
-        Svømmer.createSvømmer(name, age, isActive, competetiv);
+        database.createSvømmer(name, age, isActive, competetiv);
     }
 
 }
