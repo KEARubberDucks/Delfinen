b73a957e5a2d55966ccf89f78860adf2e58d5301
HostilePlayer <mart5e86@stud.kea.dk>
2022-11-28 16:22:07 +0100

    fix by Wenmin

--- src/main/java/Comparators/TypeComparator.java
+++ src/main/java/Comparators/TypeComparator.java
@@ -1,15 +1,12 @@
 package Comparators;
 
-import FileAndDatabase.Database;
 import MainClasses.Swimmer;
-
 import java.util.Comparator;
 
 public class TypeComparator implements Comparator<Swimmer> {
 
     @Override
-    public int compare(Swimmer o1, Swimmer o2) {
-        return o1.getIsCompetetiv().compareTo(o2.getIsCompetetiv());
+    public int compare(Swimmer o1, Swimmer o2){
+        return Boolean.compare(o1.getIsCompetetiv(), o2.getIsCompetetiv());
     }
-
 }
