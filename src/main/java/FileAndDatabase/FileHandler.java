fc18650b7c6f20e8c665616d1395d29f0524ddfb
HostilePlayer <mart5e86@stud.kea.dk>
2022-12-05 04:21:55 +0100

    CompetitiveSwimmer kan nu loades

--- src/main/java/FileAndDatabase/FileHandler.java
+++ src/main/java/FileAndDatabase/FileHandler.java
@@ -1,4 +1,5 @@
 package FileAndDatabase;
+
 import MainClasses.CompetitiveSwimmer;
 import MainClasses.Swimmer;
 
@@ -11,40 +12,64 @@ import java.util.Scanner;
 
 public class FileHandler {
     File file;
+    File file2;
     PrintStream output;
+    PrintStream output2;
     Scanner input;
-    public FileHandler(){
+
+    public FileHandler() {
         file = new File("Resources/Svømmer.txt");
+        file2 = new File("Resources/CompetitiveSvømmer.txt");
     }
+
     //Swimmere skal med lille s
-    public void saveSwimmers(ArrayList<Swimmer> Swimmere)throws FileNotFoundException{
+    public void saveSwimmers(ArrayList<Swimmer> Swimmere) throws FileNotFoundException {
         output = new PrintStream(file);
-        if (!Swimmere.isEmpty()){
-            for (Swimmer swimmer : Swimmere){
-                    if (swimmer instanceof CompetitiveSwimmer){
-                        output.println(swimmer.getName()+ "; "+swimmer.getAge()+ "; " + swimmer.getIsActive()+ "; " + swimmer.getIsCompetitive()+ "; " +
-                                        swimmer.getSwimmingDisciplines()+ "; " + swimmer.getBestResult()+ "; " + swimmer.getDateOfResult()+ "; " +
-                                swimmer.getCompetitionOfResults()+ "; " + swimmer.getPlaceOfResult()+ "; " + swimmer.getTrainer());
-                    } else {
-                        output.println(swimmer.getName()+ "; "+swimmer.getAge()+ "; " + swimmer.getIsActive()+ "; " + swimmer.getIsCompetitive());
-                    }
+        output2 = new PrintStream(file2);
+        if (!Swimmere.isEmpty()) {
+            for (Swimmer swimmer : Swimmere) {
+                if (swimmer instanceof CompetitiveSwimmer) {
+                    output2.println(swimmer.getName() + "; " + swimmer.getAge() + "; " + swimmer.getIsActive() + "; " + swimmer.getIsCompetitive() + "; " +
+                            swimmer.getSwimmingDisciplines() + "; " + swimmer.getBestResult() + "; " + swimmer.getDateOfResult() + "; " +
+                            swimmer.getCompetitionOfResults() + "; " + swimmer.getPlaceOfResult() + "; " + swimmer.getTrainer());
+                } else {
+                    output.println(swimmer.getName() + "; " + swimmer.getAge() + "; " + swimmer.getIsActive() + "; " + swimmer.getIsCompetitive());
+                }
             }
         }
     }
-    public ArrayList<Swimmer> loadSvømmer() throws FileNotFoundException{
+
+    public ArrayList<Swimmer> loadSvømmer() throws FileNotFoundException {
         ArrayList<Swimmer> returnList = new ArrayList<>();
-        Swimmer svømmerToAdd;
+        Swimmer swimmerToAdd;
         input = new Scanner(file);
-        while (input.hasNextLine()){
+        while (input.hasNextLine()) {
             String[] attributeList = input.nextLine().split("; ");
-            svømmerToAdd = new Swimmer(
+
+            swimmerToAdd = new Swimmer(
                     attributeList[0],
                     Integer.parseInt(attributeList[1]),
                     attributeList[2].equals("ja"),
                     attributeList[3].equals("ja")
-
             );
-            returnList.add(svømmerToAdd);
+            returnList.add(swimmerToAdd);
+        }
+        input = new Scanner(file2);
+        while (input.hasNextLine()) {
+            String[] attributeList = input.nextLine().split("; ");
+            swimmerToAdd = new CompetitiveSwimmer(
+                    attributeList[0],
+                    Integer.parseInt(attributeList[1]),
+                    attributeList[2].equals("ja"),
+                    attributeList[3].equals("ja"),
+                    attributeList[4],
+                    attributeList[5],
+                    attributeList[6],
+                    Integer.parseInt(attributeList[7]),
+                    attributeList[8],
+                    attributeList[9]
+            );
+            returnList.add(swimmerToAdd);
         }
         return returnList;
     }
